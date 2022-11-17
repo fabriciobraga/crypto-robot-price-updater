fa1b95fb0417ad5da60ccfac23200a102dc2e06b
Fabricio Braga <fabribraga@gmail.com>
2022-11-17 10:37:37 -0300

    versão incial, com DB configurado

---
+++ src/main/java/com/fabricio/crypto/robot/CryptoRobotPriceUpdaterApplication.java
@@ -0,0 +1,13 @@
+package com.fabricio.crypto.robot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoRobotPriceUpdaterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoRobotPriceUpdaterApplication.class, args);
+	}
+
+}
