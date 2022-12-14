8d6a379b44bbc1d05c938583aeae1063f7e8a7f4
Fabricio Braga <fabribraga@gmail.com>
2022-12-14 13:47:43 -0300

    versão funcionando

---
+++ src/main/java/com/fabricio/crypto/robot/util/Test.java
@@ -0,0 +1,13 @@
+package com.fabricio.crypto.robot.util;
+
+public class Test {
+
+	public static void main(String[] args) {
+		
+		Integer a = new Integer (3);
+		Integer b = new Integer (3);
+		System.out.println(a == b);
+
+	}
+
+}
